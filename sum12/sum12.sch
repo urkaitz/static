ad0519354d47edf337eda9edeaac1d7ceed835ab
borja.urkizu <borja.urquizu@tecnalia.com>
2021-12-14 10:17:05 +0100

    Add alg for sum4 y sum12

---
+++ sum12/sum12.sch
@@ -0,0 +1,6 @@
+1
+1
+sum-0
+1 0
+0
+WebAssembly/target/debug/wasca /home/eneko/projects/brokel/mpc/brokel-mpc-sm/run/RustPrograms/examples/sum/wasca.wasm /home/eneko/projects/brokel/mpc/brokel-mpc-sm/run/RustPrograms/examples/sum/sum.bc 
---
+++ sum4/main.rs
@@ -0,0 +1,31 @@
+// Copyright (c) 2021, Tecnalia RI, Derio, Bizkaia.
+// Inaki Seco Aguirre <inaki.seco@tecnalia.com>
+
+#![no_std]
+#![no_main]
+#![feature(const_evaluatable_checked)]
+
+use scale_std::local_functions::JSON_set_key;
+
+#[scale::main(KAPPA = 40)]
+#[inline(always)]
+
+fn main() {
+    println!("\n# Sum ALGORITHM");
+
+    const PARTY_LEN:i64 = 3;
+    const DATA_LEN:i64 = 4;
+
+    let mut sum = SecretModp::from(0);
+
+    JSON_set_key("salary");
+    
+    for i in 1..PARTY_LEN { 
+        for _j in 0..DATA_LEN {
+            let input = SecretModp::private_input(i, 10);
+            sum = sum + input;
+        }
+    }
+
+    sum.private_output(0, 10);
+}
